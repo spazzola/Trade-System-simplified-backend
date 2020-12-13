19e703e92e45e75773132be281516a88f966e278
Rafal Szczotka <rafalszczotka@wp.pl>
2020-12-13 21:59:35 +0000

    Project initialized

---
+++ src/main/java/tradesystemsymplified/TradesystemsymplifiedApplication.java
@@ -0,0 +1,13 @@
+package tradesystemsymplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesystemsymplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradesystemsymplifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tradesystemsymplified/TradesystemsymplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package tradesystemsymplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradesystemsymplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
