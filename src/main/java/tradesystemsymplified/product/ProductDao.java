9eeec5dac0429d14ba815c5c751b8b5cb7891766
Rafal Szczotka <rafalszczotka@wp.pl>
2020-12-24 16:46:16 +0000

    Added Product model class, controller, dao, dto, mapper, service

---
+++ src/main/java/tradesystemsymplified/product/ProductDao.java
@@ -0,0 +1,11 @@
+package tradesystemsymplified.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductDao extends JpaRepository<Product, Long> {
+
+    Product findByProduct(String product);
+
+}
---
+++ src/main/java/tradesystemsymplified/product/ProductMapper.java
@@ -0,0 +1,24 @@
+package tradesystemsymplified.product;
+
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ProductMapper {
+
+    public ProductDto toDto(Product product) {
+        return ProductDto.builder()
+                .product(product.getProduct())
+                .id(product.getId())
+                .build();
+    }
+
+    public List<ProductDto> toDto(List<Product> invoices) {
+        return invoices.stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+
+    }
+
+}
